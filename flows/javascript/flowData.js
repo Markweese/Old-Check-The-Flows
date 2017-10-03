bf515e8b66a6540d840be213b2cf03fa8fb6e1bb
Marquis <theusual@icloud.com>
2017-10-03 14:34:23 -0600

    Testing Map Plugin
    Testing Google map plugin. This is probably a sign that you should
    start using a better version control work flow as well ;)

--- flows/javascript/flowData.js
+++ flows/javascript/flowData.js
@@ -1,8 +1,11 @@
 var spots = [{}];
-var spec = "co";
+var spec = "";
 var checkVar;
 var arrLength;
 var myArr;
+//map http request
+var xhr = new XMLHttpRequest();
+//list http request
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -46,3 +49,58 @@ xmlhttp.onreadystatechange = function() {
 //xmlhttp.open("GET", "https://waterservices.usgs.gov/nwis/dv/?format=json&sites=09037500,09080400,06700000,09132500,09046490,06620000,06730200,06741510,06751490&siteType=ST&siteStatus=active", true);
 xmlhttp.open("GET", "https://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=" + spec + "&parameterCd=00060,00065&siteType=ST&siteStatus=active", true);
 xmlhttp.send();
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 42.001442, lng: -111.099978},
+    zoom: 6
+  });
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myArr = JSON.parse(this.responseText);
+
+        myArr.value.timeSeries.forEach(function (data){
+          if(data.variable.variableName == "Streamflow, ft&#179;/s"){
+          var latLng = new google.maps.LatLng(data.sourceInfo.geoLocation.geogLocation.latitude, data.sourceInfo.geoLocation.geogLocation.longitude);
+          var marker = new google.maps.Marker({
+           position: latLng,
+           map: map,
+           title: data.FIELD1
+          });
+          var details = "<h1>" + data.sourceInfo.siteName + "</h1> <h2> Running at: <b>" + data.values[0].value[0].value + " CFS</b></h2><button id=\"addRiver\">Add To List</button>";
+          bindInfoWindow(marker, map, new google.maps.InfoWindow(), details);
+          }
+        });
+
+        function bindInfoWindow(marker, map, infowindow, strDescription) {
+          google.maps.event.addListener(marker, 'click', function () {
+          infowindow.setContent(strDescription);
+          infowindow.open(map, marker);
+          });
+        }
+      }
+    }
+    xhr.open("GET", "https://raw.githubusercontent.com/Markweese/Check-The-Flows/master/flows/testQuery.json", true);
+    xhr.send();
+  }
+
+  function openMap() {
+    var mapNode = document.createElement("div");
+    var textNode = document.createTextNode("");
+    document.getElementById("openMap").style.display = "none";
+    document.getElementById("closeMap").style.display = "block";
+    mapNode.appendChild(textNode);
+    mapNode.id = "map";
+    document.getElementById("dad").appendChild(mapNode);
+    initMap();
+
+  }
+
+  function shutMap(){
+    var parent = document.getElementById("dad");
+    var child = document.getElementById("map");
+    document.getElementById("openMap").style.display = "block";
+    document.getElementById("closeMap").style.display = "none";
+    parent.removeChild(child);
+  }
